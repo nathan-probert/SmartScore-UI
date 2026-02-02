566d67bd1af025756ed34acc77427a297d5619ce
Nathan Probert <nathanprobert@rogers.com>
2026-02-01 21:18:04 -0500

    Try this

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 export default {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
   reactStrictMode: true,
-  output: 'standalone',
   basePath: '',  // No base path for local or production. Keep it at the root level.
   assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',  // Don't change the asset prefix; keep it the same for development and production
   async rewrites() {
