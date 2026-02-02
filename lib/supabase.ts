ca2b77127f278d0563ba189f98543d7541049c5b
Nathan Probert <nathanprobert@rogers.com>
2026-02-02 17:52:26 -0500

    Add null check

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Missing Supabase environment variables: NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_API_KEY');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
